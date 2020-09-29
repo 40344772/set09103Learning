77abc5a6a3a6d7348bb5f844dbc7826289422e21
40344772 <40344772@live.napier.ac.uk>
2020-09-29 13:03:22 +0100

    chapter 3 work To Finish

--- hello.py
+++ hello.py
@@ -13,6 +13,10 @@ def hello():
 def goodbye():
 	return "Goodbye cruel world :("
 
+@app.errorhandler(404)
+def page_not_found(error):
+	return "This is not the page you are looking for", 404
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
 
